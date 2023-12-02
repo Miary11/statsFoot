ffb4919d5957fb13e21828b9cf00f0447bf256cc
Miary <Miaryvaliniavo@gmail.com>
2023-12-02 16:08:27 +0300

    c

--- webService-node-version/statsController.js
+++ webService-node-version/statsController.js
@@ -1,10 +1,14 @@
 const { Pool } = require('pg');
 
-const neonConnectionString = "postgresql://Miary11:JS1wUeKQ2Trt@ep-divine-cake-65130730-pooler.us-east-2.aws.neon.tech/statsfoot?sslmode=require";
-const modifiedConnectionString = neonConnectionString.replace('-pooler.', '.'); // Remove the '-pooler' part
-
 const pool = new Pool({
-  connectionString: modifiedConnectionString,
+  host: 'statsfoot-miaryvaliniavo-5046.a.aivencloud.com',
+  port: 15819,
+  database: 'statsfoot',
+  user: 'avnadmin',
+  password: 'AVNS_wGgLPbTcF-KyUpJAXCR',
+  ssl: {
+    rejectUnauthorized: false, // You may need to set this to 'true' if Aiven uses a self-signed certificate
+  },
 });
 
 async function getStatsGeneral(req, res) {
@@ -12,16 +16,16 @@ async function getStatsGeneral(req, res) {
     const client = await pool.connect();
     const result = await client.query('SELECT * FROM statsEquipeGeneralGeneral');
     const allStatsGen = result.rows.map((row) => ({
-      Equipe: row[0],
-      Ligue: row[1],
-      Buts: row[2],
-      TirsMatch: row[3],
-      CartonJaune: row[4],
-      CartonRouge: row[5],
-      Possession: row[6],
-      PassesReussies: row[7],
-      AerienGagne: row[8],
-      Note: row[9],
+      Equipe: row.equipe,
+      Ligue: row.ligue,
+      Buts: row.buts,
+      TirsMatch: row.tirmatch,
+      CartonJaune: row.cartonjaune,
+      CartonRouge: row.cartonrouge,
+      Possession: row.possession,
+      PassesReussies: row.passesreussies,
+      AerienGagne: row.aeriengagnes,
+      Note: row.note,
     }));
 
     // Set CORS headers
@@ -42,16 +46,16 @@ async function getStatsGeneralDomicile(req, res) {
     const client = await pool.connect();
     const result = await client.query('select * from statsEquipeGeneralDomicile');
     const allStatsGen = result.rows.map((row) => ({
-      Equipe: row[0],
-      Ligue: row[1],
-      Buts: row[2],
-      TirsMatch: row[3],
-      CartonJaune: row[4],
-      CartonRouge: row[5],
-      Possession: row[6],
-      PassesReussies: row[7],
-      AerienGagne: row[8],
-      Note: row[9],
+      Equipe: row.equipe,
+      Ligue: row.ligue,
+      Buts: row.buts,
+      TirsMatch: row.tirmatch,
+      CartonJaune: row.cartonjaune,
+      CartonRouge: row.cartonrouge,
+      Possession: row.possession,
+      PassesReussies: row.passesreussies,
+      AerienGagne: row.aeriengagnes,
+      Note: row.note,
     }));
 
     // Set CORS headers
@@ -72,16 +76,16 @@ async function getStatsGeneralExterieur(req, res) {
     const client = await pool.connect();
     const result = await client.query('select * from statsEquipeGeneralExterieur');
     const allStatsGen = result.rows.map((row) => ({
-      Equipe: row[0],
-      Ligue: row[1],
-      Buts: row[2],
-      TirsMatch: row[3],
-      CartonJaune: row[4],
-      CartonRouge: row[5],
-      Possession: row[6],
-      PassesReussies: row[7],
-      AerienGagne: row[8],
-      Note: row[9],
+      Equipe: row.equipe,
+      Ligue: row.ligue,
+      Buts: row.buts,
+      TirsMatch: row.tirmatch,
+      CartonJaune: row.cartonjaune,
+      CartonRouge: row.cartonrouge,
+      Possession: row.possession,
+      PassesReussies: row.passesreussies,
+      AerienGagne: row.aeriengagnes,
+      Note: row.note,
     }));
 
     // Set CORS headers
@@ -102,14 +106,14 @@ async function getStatsDefenseDomicile(req, res) {
     const client = await pool.connect();
     const result = await client.query('select * from statsEquipeDefenseDomicile');
     const allStatsGen = result.rows.map((row) => ({
-      Equipe: row[0],
-      Ligue: row[1],
-      TirsMatch: row[2],
-      TacleMatch: row[3],
-      InterceptionMatch: row[4],
-      FauteMatch: row[5],
-      HorsJeuMatch: row[6],
-      Note: row[7],
+      Equipe: row.equipe,
+      Ligue: row.ligue,
+      TirsMatch: row.tirmatch,
+      TacleMatch: row.taclematch,
+      InterceptionMatch: row.interceptionmatch,
+      FauteMatch: row.fautematch,
+      HorsJeuMatch: row.horsjeuxmatch,
+      Note: row.note,
     }));
 
     // Set CORS headers
@@ -130,14 +134,14 @@ async function getStatsDefenseExterieur(req, res) {
     const client = await pool.connect();
     const result = await client.query('select * from statsEquipeDefenseExterieur');
     const allStatsGen = result.rows.map((row) => ({
-      Equipe: row[0],
-      Ligue: row[1],
-      TirsMatch: row[2],
-      TacleMatch: row[3],
-      InterceptionMatch: row[4],
-      FauteMatch: row[5],
-      HorsJeuMatch: row[6],
-      Note: row[7],
+      Equipe: row.equipe,
+      Ligue: row.ligue,
+      TirsMatch: row.tirmatch,
+      TacleMatch: row.taclematch,
+      InterceptionMatch: row.interceptionmatch,
+      FauteMatch: row.fautematch,
+      HorsJeuMatch: row.horsjeuxmatch,
+      Note: row.note,
     }));
 
     // Set CORS headers
@@ -158,14 +162,14 @@ async function getStatsDefenseGeneral(req, res) {
     const client = await pool.connect();
     const result = await client.query('select * from statsEquipeDefenseGeneral');
     const allStatsGen = result.rows.map((row) => ({
-      Equipe: row[0],
-      Ligue: row[1],
-      TirsMatch: row[2],
-      TacleMatch: row[3],
-      InterceptionMatch: row[4],
-      FauteMatch: row[5],
-      HorsJeuMatch: row[6],
-      Note: row[7],
+      Equipe: row.equipe,
+      Ligue: row.ligue,
+      TirsMatch: row.tirmatch,
+      TacleMatch: row.taclematch,
+      InterceptionMatch: row.interceptionmatch,
+      FauteMatch: row.fautematch,
+      HorsJeuMatch: row.horsjeuxmatch,
+      Note: row.note,
     }));
 
     // Set CORS headers
@@ -186,13 +190,13 @@ async function getStatsAttaqueDomicile(req, res) {
     const client = await pool.connect();
     const result = await client.query('select * from statsEquipeAttaqueDomicile');
     const allStatsGen = result.rows.map((row) => ({
-      Equipe: row[0],
-      Ligue: row[1],
-      TirsMatch: row[2],
-      TirsCadreMatch: row[3],
-      DribbleMatch: row[4],
-      FauteSubieMatch: row[5],
-      Note: row[6],
+      Equipe: row.equipe,
+      Ligue: row.ligue,
+      TirsMatch: row.tirmatch,
+      TirsCadreMatch: row.tircadrematch,
+      DribbleMatch: row.dribblematch,
+      FauteSubieMatch: row.fautesubiematch,
+      Note: row.note,
     }));
 
     // Set CORS headers
@@ -213,13 +217,13 @@ async function getStatsAttaqueExterieur(req, res) {
     const client = await pool.connect();
     const result = await client.query('select * from statsEquipeAttaqueExterieur');
     const allStatsGen = result.rows.map((row) => ({
-      Equipe: row[0],
-      Ligue: row[1],
-      TirsMatch: row[2],
-      TirsCadreMatch: row[3],
-      DribbleMatch: row[4],
-      FauteSubieMatch: row[5],
-      Note: row[6],
+      Equipe: row.equipe,
+      Ligue: row.ligue,
+      TirsMatch: row.tirmatch,
+      TirsCadreMatch: row.tircadrematch,
+      DribbleMatch: row.dribblematch,
+      FauteSubieMatch: row.fautesubiematch,
+      Note: row.note,
     }));
 
     // Set CORS headers
@@ -240,13 +244,13 @@ async function getStatsAttaqueGeneral(req, res) {
     const client = await pool.connect();
     const result = await client.query('select * from statsEquipeAttaqueGeneral');
     const allStatsGen = result.rows.map((row) => ({
-      Equipe: row[0],
-      Ligue: row[1],
-      TirsMatch: row[2],
-      TirsCadreMatch: row[3],
-      DribbleMatch: row[4],
-      FauteSubieMatch: row[5],
-      Note: row[6],
+      Equipe: row.equipe,
+      Ligue: row.ligue,
+      TirsMatch: row.tirmatch,
+      TirsCadreMatch: row.tircadrematch,
+      DribbleMatch: row.dribblematch,
+      FauteSubieMatch: row.fautesubiematch,
+      Note: row.note,
     }));
 
     // Set CORS headers
