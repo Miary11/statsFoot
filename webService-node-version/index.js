177997b6bfd24bc9cc78776a3298dba853b00297
Miary <Miaryvaliniavo@gmail.com>
2023-12-02 10:54:09 +0300

    Update index.js

--- webService-node-version/index.js
+++ webService-node-version/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const statsController = require('./statsController');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.get('/statsGeneral', statsController.getStatsGeneral);
 app.get('/statsGeneralDomicile', statsController.getStatsGeneralDomicile);
